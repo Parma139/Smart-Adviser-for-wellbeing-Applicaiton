b2e851c2d61473d3840ecbcf83ef4918c902074e
Parma <zp17aan@herts.ac.uk>
2021-03-04 20:41:04 +0000

    New Pages and Buttons
    Four pages and buttons were added: Notes, General, time management, and Anxiety to the application.

--- app/src/main/java/com/example/smart_adviser_for_wellbeing/MainActivity.java
+++ app/src/main/java/com/example/smart_adviser_for_wellbeing/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.smart_adviser_for_wellbeing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonAnxiety, buttonNotes, buttonTimeManagement, buttonGeneral;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+           buttonAnxiety=findViewById(R.id.Anxiety);
+           buttonNotes=findViewById(R.id.Notes);
+           buttonTimeManagement=findViewById(R.id.TimeManagement);
+           buttonGeneral=findViewById(R.id.General);
+
+           buttonAnxiety.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                 FragmentManager  fm = getSupportFragmentManager();
+                 fm.beginTransaction().replace(R.id.main, new Anxiety()) .addToBackStack(null).commit();
+
+               }
+           });
+
+
+        buttonNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager  fm = getSupportFragmentManager();
+                fm.beginTransaction().replace(R.id.main, new Notes()) .addToBackStack(null).commit();
+
+            }
+          });
+
+        buttonTimeManagement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager  fm = getSupportFragmentManager();
+                fm.beginTransaction().replace(R.id.main, new TimeMnangement()) .addToBackStack(null).commit();
+
+            }
+           });
+
+        buttonGeneral.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager  fm = getSupportFragmentManager();
+                fm.beginTransaction().replace(R.id.main, new General()) .addToBackStack(null).commit();
+
+            }
+            });
     }
 }
