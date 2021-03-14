2e677269e9fbdd3e1fea58668f6035c645e8720d
Parma <zp17aan@herts.ac.uk>
2021-03-14 01:00:58 +0000

    Image button has been updated
    Icon button or image button has been redesigned and unnecessary code has been removed from all the java class.

--- app/src/main/java/com/example/smart_adviser_for_wellbeing/Anxiety.java
+++ app/src/main/java/com/example/smart_adviser_for_wellbeing/Anxiety.java
@@ -8,48 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+
 public class Anxiety extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-//    public Anxiety() {
-//        // Required empty public constructor
-//    }
-
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment Anxiety.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static Anxiety newInstance(String param1, String param2) {
-//        Anxiety fragment = new Anxiety();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public Anxiety() {
+        // Required empty public constructor
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
--- app/src/main/java/com/example/smart_adviser_for_wellbeing/MainActivity.java
+++ app/src/main/java/com/example/smart_adviser_for_wellbeing/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.smart_adviser_for_wellbeing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,47 +19,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         ImageButton homeButton = (ImageButton)findViewById(R.id.homeButton);
-         ImageButton noteButton = (ImageButton)findViewById(R.id.noteButton);
-         ImageButton  anxietyButton = (ImageButton)findViewById(R.id.anxietyButton);
-         ImageButton timemanagementButton = (ImageButton)findViewById(R.id.timemanagementButon);
+        // btnnav start
 
+        BottomNavigationView btnNav = findViewById(R.id.bottomNavigationview);
+        btnNav.setOnNavigationItemSelectedListener(navListener);
 
+        // Setting Home Fragment as main Fragment
+         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentscreen_layout, new General()).commit();
 
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager  fm = getSupportFragmentManager();
-                fm.beginTransaction().replace(R.id.main, new General()) .addToBackStack(null).commit();
-            }
-        });
-
-        noteButton.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View v) {
-                 FragmentManager  fm = getSupportFragmentManager();
-                 fm.beginTransaction().replace(R.id.main, new Notes()) .addToBackStack(null).commit();
-
-               }
-            });
-
-        anxietyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager  fm = getSupportFragmentManager();
-                fm.beginTransaction().replace(R.id.main, new Anxiety()) .addToBackStack(null).commit();
-            }
-        });
-
-
-        timemanagementButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager  fm = getSupportFragmentManager();
-                fm.beginTransaction().replace(R.id.main, new TimeMnangement()) .addToBackStack(null).commit();
-            }
-        });
+        // end betnav
 
     }
+
+    // nav start
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.item1:
+                    selectedFragment = new General();
+                    break;
+                case R.id.item2:
+                    selectedFragment = new Notes();
+                    break;
+
+                case R.id.item3:
+                    selectedFragment = new Anxiety();
+                    break;
+
+                case R.id.item4:
+                    selectedFragment = new TimeMnangement();
+                    break;
+
+            }
+
+            //Begin transisiton
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentscreen_layout, selectedFragment).addToBackStack(null).commit();
+            return true;
+
+        }
+    };
+    // nav end
 
 }
